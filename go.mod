c80aa0316f772be309c066e6ec6f0f4f4b16a131
Tanmay Madaan <tanmaymadaan@outlook.com>
2023-09-15 00:31:43 +0530

    feat: add konseouldb setup

--- go.mod
+++ go.mod
@@ -3,5 +3,6 @@ module queen-bee
 go 1.20
 
 require (
+	github.com/joho/godotenv v1.5.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -2,22 +2,47 @@ package main
 
 import (
 	"fmt"
+	"github.com/joho/godotenv"
 	"gopkg.in/yaml.v3"
 	"log"
 	"os"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	numContainers := 5 // Change this to the desired number of containers
 
 	composeData := map[string]interface{}{
 		"version":  "3",
 		"services": make(map[string]interface{}),
+		"volumes":  make(map[string]interface{}),
 	}
+
+	db := make(map[string]interface{})
+	var dbEnvVars []string
+	dbEnvVars = append(dbEnvVars, fmt.Sprintf("POSTGRES_USER=%v", os.Getenv("POSTGRES_USER")))
+	dbEnvVars = append(dbEnvVars, fmt.Sprintf("POSTGRES_PASSWORD=%v", os.Getenv("POSTGRES_PASS")))
+
+	dbName := "konseouldb"
+	db["image"] = "postgres:14.1-alpine"
+	db["restart"] = "always"
+	db["environment"] = dbEnvVars
+	db["ports"] = []string{fmt.Sprintf("5432:5432")}
+	db["volumes"] = []string{fmt.Sprintf("konseouldb:/var/lib/postgresql/data")}
+	composeData["services"].(map[string]interface{})[dbName] = db
+
+	volume := make(map[string]interface{})
+	volumeName := dbName
+	volume["driver"] = "local"
+	composeData["volumes"].(map[string]interface{})[volumeName] = volume
 
 	for i := 1; i <= numContainers; i++ {
 		service := make(map[string]interface{})
-		serviceName := fmt.Sprintf("web%d", i)
+		serviceName := fmt.Sprintf("cornflake-%d", i)
 		port := 8080 + i
 
 		service["build"] = map[string]interface{}{
